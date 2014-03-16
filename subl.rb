433cc438020e7c85200875da6af83b336c57dece
oame <oame@oameya.com>
2014-03-16 17:38:53 +0900

    Update version

--- subl.rb
+++ subl.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Subl < Formula
   homepage 'https://github.com/oame/'
   url 'https://raw.github.com/oame/homebrew-subl/master/bin/subl'
-  sha1 '22901fd364d228d2df19b560ea6142d0f90ab761'
-  version '1.0'
+  sha1 'a3e1936642c8b152d10ce502555678d48ecc74e6'
+  version '1.1'
 
   def install
     bin.install 'subl' => 'subl'
